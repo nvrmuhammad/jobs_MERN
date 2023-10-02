bea43bb651a1f671dc5ed13e4dd35233d7c2a582
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-02 18:20:12 +0500

    controllerdeki xatolik togirlandi

--- server/src/modules/users/_api.js
+++ server/src/modules/users/_api.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { listUsers } from './_controller.js'
+import { addUsers, listUsers } from './_controller.js'
 
 const router = Router()
 
 router.get('/users', listUsers)
-router.post('/users')
+router.post('/users' , addUsers)
 router.get('/users/:id')
 router.put('/users/:id')
 router.post('/users/:id') // Login
