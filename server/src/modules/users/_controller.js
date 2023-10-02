5b252c409a3dbd2e068bcba0c2b73d60f350b57c
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-02 18:13:54 +0500

    users uchun controller yozildi

--- server/src/modules/users/_controller.js
+++ server/src/modules/users/_controller.js
@@ -1,3 +1,4 @@
+import { addUsersService } from './add-users.js'
 import { listUsersService } from './list-users.js'
 
 export const listUsers = (req, res, next) => {
@@ -5,6 +6,15 @@ export const listUsers = (req, res, next) => {
     const result = listUsersService()
 
     res.status(200).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
+export const addUsers = (req, res, next) => {
+  try {
+    const result = addUsersService({ body: req.body })
+
+    res.status(201).json({ data: result })
   } catch (error) {
     next(error)
   }
