a5b40e9526b8ea8232b32ba68ec2ca148d524cf7
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-03 15:38:06 +0500

    id orqali 1ta userri korish controlleri yozildi

--- server/src/modules/users/_api.js
+++ server/src/modules/users/_api.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
-import { addUsers, listUsers } from './_controller.js'
+import { addUsers, listUsers, showUsers } from './_controller.js'
 
 const router = Router()
 
 router.get('/users', listUsers)
-router.post('/users' , addUsers)
-router.get('/users/:id')
+router.post('/users', addUsers)
+router.get('/users/:id', showUsers)
 router.put('/users/:id')
 router.post('/users/:id') // Login
 router.delete('/users/:id')
--- server/src/modules/users/_controller.js
+++ server/src/modules/users/_controller.js
@@ -1,9 +1,19 @@
 import { addUsersService } from './add-users.js'
 import { listUsersService } from './list-users.js'
+import { showUsersService } from './show-user.js'
 
 export const listUsers = async (req, res, next) => {
   try {
     const result = await listUsersService()
+
+    res.status(200).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
+export const showUsers = async (req, res, next) => {
+  try {
+    const result = await showUsersService({ params: req.params })
 
     res.status(200).json({ data: result })
   } catch (error) {
