f738935c2957421e876436aed48f215bea83afb2
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-03 17:45:22 +0500

    login user controller yozildi

--- server/src/modules/users/_controller.js
+++ server/src/modules/users/_controller.js
@@ -1,5 +1,6 @@
 import { addUsersService } from './add-users.js'
 import { listUsersService } from './list-users.js'
+import { loginUsersService } from './login-users.js'
 import { removeUserService } from './remove-user.js'
 import { showUsersService } from './show-user.js'
 import { updateUserService } from './update-users.js'
@@ -43,6 +44,17 @@ export const updateUsers = async (req, res, next) => {
     next(error)
   }
 }
+
+export const loginUsers = async (req, res, next) => {
+  try {
+    const result = await loginUsersService({ body: req.body })
+
+    res.status(200).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const removeUser = async (req, res, next) => {
   try {
     const result = await removeUserService({ params: req.params })
