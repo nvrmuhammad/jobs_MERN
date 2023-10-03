5f771c8de1e04a397d5a116813ec9803f236ae5c
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-03 16:06:06 +0500

    routerdaki xatolik togirlandi

--- server/src/modules/users/_api.js
+++ server/src/modules/users/_api.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
-import { addUsers, listUsers, showUsers } from './_controller.js'
+import { addUsers, listUsers, showUsers, updateUsers } from './_controller.js'
+import { updateUserService } from './update-users.js'
 
 const router = Router()
 
 router.get('/users', listUsers)
 router.post('/users', addUsers)
 router.get('/users/:id', showUsers)
-router.put('/users/:id')
+router.put('/users/:id', updateUsers)
 router.post('/users/:id') // Login
 router.delete('/users/:id')
 
