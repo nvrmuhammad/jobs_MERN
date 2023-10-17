02b815ba76436615489b16a6cf48008b422d573d
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:33:10 +0500

    admin uchun update-admin service yozildi

--- server/src/modules/admin/update-admin.js
+++ server/src/modules/admin/update-admin.js
@@ -2,13 +2,24 @@ import mongoose from 'mongoose'
 import { Admin } from './Admin.js'
 import bcrypt from 'bcrypt'
 
-export const updateAdminService = async ({ body, id }) => {
+export const updateAdminService = async ({ body, user }) => {
   const { password } = body
+  const { id, role } = user
+
+  if (role !== 'admin') {
+    return { error: 'You are not admin' }
+  }
+
+  const checking = await Admin.findOne({ _id: id })
+
+  if (!checking) {
+    return { error: 'Admin is not defined ' }
+  }
 
   const hashedPassword = bcrypt.hashSync(password, 10)
 
   const updatedAdmin = await Admin.findByIdAndUpdate(
-    { _id: id.id },
+    { _id: id },
     { ...body, password: hashedPassword },
     { new: true }
   )
