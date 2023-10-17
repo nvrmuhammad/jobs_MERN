8511f5d5491a0315d5ffdaaa2579a52a945be76c
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:32:05 +0500

    list-admin service yozildi

--- server/src/modules/admin/list-admin.js
+++ server/src/modules/admin/list-admin.js
@@ -1,6 +1,17 @@
 import { Admin } from './Admin.js'
 
-export const listAdminsServices = async () => {
+export const listAdminsServices = async ({ user }) => {
+  const { role, id } = user
+
+  const checking = await Admin.findOne({ _id: id })
+
+  if (!checking) {
+    return { error: 'admin is not defined' }
+  }
+  if (role !== 'admin') {
+    return { error: 'you have not access' }
+  }
+
   const list = await Admin.find()
 
   return list
