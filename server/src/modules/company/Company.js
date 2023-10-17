23d070eb97e49b82c1f8eccf726e3d4790a21143
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:34:07 +0500

    Company uchun Mongo Schema model yozildi

--- server/src/modules/company/Company.js
+++ server/src/modules/company/Company.js
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose'
 
 const CompanySchema = new mongoose.Schema({
+  email: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  password: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
   name: {
     type: mongoose.SchemaTypes.String,
     required: true,
@@ -14,15 +22,33 @@ const CompanySchema = new mongoose.Schema({
     ref: 'company_category',
     required: true,
   },
-  workers: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'users',
-    default: null,
-  },
+  workers: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'users',
+    },
+    { default: null },
+  ],
   info: {
     type: mongoose.SchemaTypes.String,
     required: true,
   },
+  is_allow: {
+    type: mongoose.SchemaTypes.Boolean,
+    default: false,
+  },
+  avatar: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
 })
+
+// {
+//   timestamps: {
+//     createdAt: 'created_at',
+//     updatedAt: 'updated_at',
+//   },
+//   versionKey: false,
+// }
 
 export const Company = mongoose.model('company', CompanySchema)
