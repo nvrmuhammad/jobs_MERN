a2f8c9e1cc4e2d5997cf7aa29278426418ab7ce6
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:38:26 +0500

    company service , loginCompany yozildi

---
+++ server/src/modules/company/login-company.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { Company } from './Company.js'
+
+export const loginCompanyService = async ({ body, user }) => {
+  const { email, password } = body
+
+  if (!email && !password) {
+    return { error: 'Please enter full' }
+  }
+
+  const company = await Company.findOne({
+    email: email,
+  })
+
+  if (!company) {
+    return { error: 'Company not found or not registered' }
+  }
+
+  const verifyPass = bcrypt.compareSync(password, company.password)
+
+  if (!verifyPass) {
+    return { error: 'Password inccorrect' }
+  }
+
+  if (company.is_allow) {
+    return {
+      error:
+        'Company is not allowed by adminstrators , please wait for some time',
+    }
+  }
+
+  const token = jwt.sign(
+    { id: company._id, role: 'company' },
+    process.env.SECRET_KEY,
+    { expiresIn: '1d' }
+  )
+
+  return token
+}
