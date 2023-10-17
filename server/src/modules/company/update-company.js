674225199a33d036f37a12c860267985b7d5af05
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:39:19 +0500

    Create update-company.js

---
+++ server/src/modules/company/update-company.js
@@ -0,0 +1,31 @@
+import { Company } from '../company/Company.js'
+import bcrypt from 'bcrypt'
+
+export const updateCompanyService = async ({ body, user, file }) => {
+  const { password } = body
+  const { id, role } = user
+  const { filename } = file
+
+  const linkAvatar = `http://localhost:2000/${filename}`
+
+  if (role !== 'company') {
+    return { error: 'You are not company admin' }
+  }
+
+  const checking = await Company.findOne({ _id: id })
+
+  if (!checking) {
+    return { error: 'Company is not defined ' }
+  }
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  const updatedCompany = await Company.findByIdAndUpdate(
+    { _id: id },
+    { ...body, avatar: linkAvatar, password: hashedPassword },
+    { new: true }
+  ).populate([
+    {
+      path: 'category_id',
+    },
+  ])
+  return updatedCompany
+}
