846c614a512c6a8519e7fe99d466cbbef81cd46b
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-17 10:39:07 +0500

    companu service , removeCompany yozildi

---
+++ server/src/modules/company/remove-company.js
@@ -0,0 +1,19 @@
+import { Company } from './Company.js'
+
+export const removeCompanyService = async ({ params, user }) => {
+  const { role } = user
+  const { id: identify } = params
+
+  if (role !== 'admin') {
+    return { error: 'You are not admin , you have not access ' }
+  }
+
+  const checking = await Company.findOne({ _id: identify })
+
+  if (!checking) {
+    return { error: 'Company is not defined or not existed' }
+  }
+
+  const removeAdmin = await Company.findByIdAndDelete({ _id: identify })
+  return removeAdmin
+}
