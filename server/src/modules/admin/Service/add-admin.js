c3f8886c80a9791598ace23cd405a761ea441d64
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-18 20:22:01 +0500

    addAdmin service folderga kochirldi

---
+++ server/src/modules/admin/Service/add-admin.js
@@ -0,0 +1,27 @@
+import { Admin } from '../Schema/Admin.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+export const addAdminService = async ({ body, user }) => {
+  const { username, password } = body
+  const { role, id } = user
+  if (role !== 'admin') {
+    return { error: 'You are not admin' }
+  }
+
+  const checking = await Admin.findOne({ _id: id })
+
+  if (!checking) {
+    return { error: 'Admin is not defined' }
+  }
+
+  const exsited = await Admin.findOne({ username: username })
+
+  if (exsited) {
+    return { error: 'This is admin already existed' }
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10)
+  const newAdmin = await Admin.create({ ...body, password: hashedPassword })
+  return newAdmin
+}
