515c3febec487349a0a5a25fed3cd4cecb07358d
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-21 08:56:41 +0500

    loginUser service yozildi

---
+++ server/src/modules/users/Service/login-users.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { Users } from '../Schema/Users.js'
+
+export const loginUsersService = async ({ body }) => {
+  const { username, password } = body
+
+  const user = await Users.findOne({
+    username: username,
+  })
+
+  if (!user) {
+    return { error: 'This user is not registered or not found' }
+  }
+
+  const verifyPass = bcrypt.compareSync(password, user.password)
+
+  if (!verifyPass) {
+    return 'Password inccorrect'
+  }
+
+  const token = jwt.sign({ id: user._id, role: 'user' }, process.env.SECRET_KEY)
+
+  return token
+}
