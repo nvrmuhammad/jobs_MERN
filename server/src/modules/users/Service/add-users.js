864c3035a19cccfcc3ac7bc50dfd9b40f5b1878e
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-21 08:56:08 +0500

    addUser service yozildi

---
+++ server/src/modules/users/Service/add-users.js
@@ -0,0 +1,24 @@
+import { Users } from '../Schema/Users.js'
+import bcrypt from 'bcrypt'
+
+export const addUsersService = async ({ body, file }) => {
+  const { username, password } = body
+  const { filename } = file
+
+  const linkAvatar = `/${filename}`
+
+  const exsited = await Users.findOne({ username: username })
+  if (exsited) {
+    return 'This is username already existed'
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10)
+
+  const newUser = await Users.create({
+    ...body,
+    password: hashedPassword,
+    avatar: linkAvatar,
+  })
+
+  return newUser
+}
