25fb3b8c6bc1e5c36bb2542fa4bce07b92d0962a
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-21 08:54:59 +0500

    addRequest controller yozildi

--- server/src/modules/request_job/_controller.js
+++ server/src/modules/request_job/_controller.js
@@ -7,3 +7,12 @@ export const listRequest = async (req, res, next) => {
     next(error)
   }
 }
+export const addRequest = async (req, res, next) => {
+  try {
+    const result = await addRequestService({ body: req.body, user: req.user })
+
+    res.status(201).json({ data: result })
+  } catch (error) {
+    next(error)
+  }
+}
