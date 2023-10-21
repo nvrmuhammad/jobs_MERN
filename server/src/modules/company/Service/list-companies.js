d21cd8ae35763a89d5fcb52b6532c8e653f575a1
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-21 08:49:39 +0500

    listCompany ga ozgartirish kiritildi

--- server/src/modules/company/Service/list-companies.js
+++ server/src/modules/company/Service/list-companies.js
@@ -1,16 +1,29 @@
 import { Company } from '../Schema/Company.js'
 
 export const listCompaniesServices = async ({ user }) => {
-  const { role } = user
+  const { role, id } = user
 
-  if (role !== 'admin') {
+  if (role == 'user') {
     return { error: 'you have not access' }
+  }
+  if (role == 'company') {
+    const show = await Company.findOne({ _id: id }).populate([
+      {
+        path: 'category_id',
+        select: 'name _id',
+      },
+    ])
   }
 
   const list = await Company.find().populate([
     {
       path: 'category_id',
       select: 'name _id',
+    },
+    {
+      path: 'workers',
+      select: 'username',
+      
     },
   ])
 
