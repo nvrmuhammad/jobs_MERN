27e3bf0dc7a711d7558af28c13db4efbabeeaebe
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-21 08:55:48 +0500

    listRequest service yozildi

---
+++ server/src/modules/request_job/Service/list-request.js
@@ -0,0 +1,29 @@
+import { Request } from '../Schema/Request.js'
+
+export const listRequestServices = async ({ user }) => {
+  const { role, id } = user
+  const checking4User = await Request.find({ user_id: id }).populate([
+    { path: 'job_id', select: 'name id' },
+    { path: 'user_id', select: 'username id' },
+    { path: 'company_id', select: 'name id' },
+  ])
+  const checking4Company = await Request.find({ company_id: id }).populate([
+    { path: 'job_id', select: 'name id' },
+    { path: 'user_id', select: 'username id' },
+    { path: 'company_id', select: 'name id' },
+  ])
+
+  if (role == 'user') {
+    return checking4User
+  }
+  if (role == 'company') {
+    return checking4Company
+  }
+
+  const listRequest = await Request.find().populate([
+    { path: 'job_id', select: 'name id' },
+    { path: 'user_id', select: 'username id' },
+    { path: 'company_id', select: 'name id' },
+  ])
+  return listRequest
+}
