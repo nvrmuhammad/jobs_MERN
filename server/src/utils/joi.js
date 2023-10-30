ac704bb7872bb6407bfead4c22eed879efcb2d0f
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-31 00:12:43 +0500

    Joi validator ishga tushirildi

--- server/src/utils/joi.js
+++ server/src/utils/joi.js
@@ -1,8 +1,20 @@
+import Joi from 'joi'
+
+// export const PASSWORD_REGEX = new RegExp(
+//   '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!.@#$%^&*])(?=.{8,})'
+// )
+
 export const authSignup = Joi.object().keys({
   first_name: Joi.required(),
   last_name: Joi.required(),
   username: Joi.required(),
   password: Joi.required(),
+})
 export const validateCategory = Joi.object().keys({
   name: Joi.string().required(),
+})
+
+export const authSignin = Joi.object().keys({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
 })
