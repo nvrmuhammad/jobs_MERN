e86062bfbdf756e70de402d9142d5bf382fa7ec5
NurMuhammad <100865232+coodnet@users.noreply.github.com>
2023-10-31 00:17:07 +0500

    response xatoligi tuzatildi

--- server/src/modules/company_category/Service/list-c-categories.js
+++ server/src/modules/company_category/Service/list-c-categories.js
@@ -3,5 +3,5 @@ import { CompanyCategory } from '../Schema/Category.js'
 export const cCategoriesService = async () => {
   const categories = await CompanyCategory.find()
 
-  return { categories, msg: 'List of company categories' }
+  return categories 
 }
